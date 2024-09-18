2051d808373c5355add013fc1aa4beb1e0ee16c4
TT1693 <taha.tasmeen@gmail.com>
2024-09-18 16:14:47 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,10 +1,11 @@
-#include<string>
-#include<vector>
-#include<numeric>
-#include<regex>
 #include<iostream>
+#include<string>
+#include <vector>
+#include <numeric>
+#include <regex>
 
 using namespace std;
 
 //int Add(const char* input);
-void Add(const string &input);
+//void Add(const string &input);
+void  Add(const string &input,void(*displayPtr)(int));
