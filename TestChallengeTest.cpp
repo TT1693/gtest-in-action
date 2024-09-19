c999f5be955b08c102b33c34e578561081cce464
TT1693 <taha.tasmeen@gmail.com>
2024-09-19 10:37:09 +0530

    Update TestChallengeTest.cpp

--- TestChallengeTest.cpp
+++ TestChallengeTest.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include "TestChallenge.h"
 #include <gmock/gmock.h>
-using testing::DoubleEq;//matcher used to asset on method arguments
+using testing::FloatEq;//matcher used to asset on method arguments
 using testing::Return; // Return value of mock function
 //Stub - Fake Dependency
 class NetworkAlerterStub:public INetworkAlerter{
